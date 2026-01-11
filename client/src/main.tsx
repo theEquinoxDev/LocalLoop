eb8f560c50966107c45ba3cbe12e7e9b7000b255
Aditya <adityasuri11ss@gmail.com>
2026-01-11 13:54:39 +0530

    init

---
+++ client/src/main.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { App } from './app/App';
+import { Providers } from './app/Providers';
+import './styles/globals.css';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <Providers>
+      <App />
+    </Providers>
+  </React.StrictMode>
+);
